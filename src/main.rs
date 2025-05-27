608eb18aab870ade63768df3b5bee0ec62a9351b
Mathias STRASSER <contact@roukmoute.fr>
2025-05-27 19:50:47 +0200

    Implement interactive loop

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@
 use std::io::{self, Write};
 
 fn main() {
-    // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
+    loop  {
+        // Uncomment this block to pass the first stage
+        print!("$ ");
+        io::stdout().flush().unwrap();
 
-    // Wait for user input
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).unwrap();
-    eprintln!("{}: command not found", input.trim());
+        // Wait for user input
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        eprintln!("{}: command not found", input.trim());
+    }
 }
